a89b407aa8d0cfc5295e612e437ab6da8a13e400
md8889 <dubemanas10@gmail.com>
2025-02-26 20:57:34 -0500

    modified yml

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { QuickstartProvider } from "./Context";
 import reportWebVitals from "./reportWebVitals";
+import { Amplify } from 'aws-amplify';
+// import awsconfig from './aws-exports';
+
+// Amplify.configure(awsconfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
